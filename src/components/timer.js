a4f49c6b316b69313fb1eb4887082c999ebcc56c
TheFox-cmd <frover997@gmail.com>
2024-05-23 23:33:38 -0500

    Fixed Img Alt Warning

--- src/components/timer.js
+++ src/components/timer.js
@@ -88,7 +88,7 @@ const Timer = () => {
       <h2>{time.minute}:{time.second}</h2>
       {
       complete 
-      ? <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fteepublic%2Fimage%2Fprivate%2Fs--sKuy4pjn--%2Fb_rgb%3Afffffe%2Ct_Heather%2520Preview%2Fc_limit%2Cf_jpg%2Ch_630%2Cq_90%2Cw_630%2Fv1533118006%2Fproduction%2Fdesigns%2F2964263_0.jpg&f=1&nofb=1&ipt=6ef0b2ecaff7b0e381545f9ce93ff8ee88079ee63210d3fbd117683af0bb26dc&ipo=images"/>
+      ? <img alt="pepeYay" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fteepublic%2Fimage%2Fprivate%2Fs--sKuy4pjn--%2Fb_rgb%3Afffffe%2Ct_Heather%2520Preview%2Fc_limit%2Cf_jpg%2Ch_630%2Cq_90%2Cw_630%2Fv1533118006%2Fproduction%2Fdesigns%2F2964263_0.jpg&f=1&nofb=1&ipt=6ef0b2ecaff7b0e381545f9ce93ff8ee88079ee63210d3fbd117683af0bb26dc&ipo=images"/>
       : <></>
       }
     </div>
